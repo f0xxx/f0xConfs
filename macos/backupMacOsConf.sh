3cd8c0444c5b410cd1a1dfee029c83cd4550d9bf
f0xxx <hunterlhy@qq.com>
2017-10-23 22:21:33 +0800

    restruct dir

---
+++ macos/backupMacOsConf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp -fv ~/.spacemacs ~/tools/conf
+cp -fv ~/.spacemacs ~/tools/conf
+cp -fv ~/.tmux.conf ~/tools/conf
+cp -fv ~/.zshrc ~/tools/conf
+cp -fv ~/.config/karabiner/karabiner.json ~/tools/conf
