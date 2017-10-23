bd4047d6f7f4b4ca3101903fc207421997bb6aa8
f0xxx <hunterlhy@qq.com>
2017-10-23 22:21:33 +0800

    restruct dir

--- copyConfs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp -fv ~/.spacemacs ~/tools/conf
-cp -fv ~/.spacemacs ~/tools/conf
-cp -fv ~/.tmux.conf ~/tools/conf
-cp -fv ~/.zshrc ~/tools/conf
-cp -fv ~/.config/karabiner/karabiner.json ~/tools/conf
